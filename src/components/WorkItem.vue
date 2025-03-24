6274ae8f04b2344d0a63dd9ddf5c16790b56fa5f
Arnaud <arnaud.masson@gimbalcube.com>
2025-03-24 11:54:35 +0100

    better ui mobile and cleaning other size

--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -16,35 +16,21 @@ defineProps<{
       alt="Work Item Background"
       name="background"
     />
-    <div class="absolute h-full w-full z-5">
-      <h3
-        class="ml-[30px] mt-[30px] md:m-0 md:absolute md:left-[10%] md:top-[12%] lg:left-[10%] lg:top-[23%] xl:left-[15%] xl:top-[25%] text-4xl md:text-5xl lg:text-6xl text-white font-bold"
-      >
+    <div class="p-[30px] h-full w-full relative z-5 flex flex-col">
+      <h3 class="text-4xl text-white font-bold">
         {{ work.name }}
       </h3>
-      <span
-        class="block ml-[30px] mt-[15px] md:m-0 md:absolute md:left-[10%] md:top-[22%] lg:left-[10%] lg:top-[32%] xl:left-[17%] xl:top-[35%] text-3xl text-black italic font-bold"
-      >
-        For {{ work.client }}
-      </span>
-      <div
-        class="ml-[30px] mt-[30px] mr-[30px] md:m-0 md:absolute md:right-[10%] md:top-[40%] lg:left-[15%] lg:top-[40%] xl:left-[19%] xl:top-[42%] md:w-[460px] text-black px-6 pt-4 pb-6 bg-white"
-      >
-        <p class="text-xs md:text-sm xl:text-lg">{{ work.description }}</p>
-        <p
-          class="text-xs md:text-sm xl:text-lg mb-2 underline underline-offset-8 decoration-dotted mt-2 md:mt-4"
-        >
-          Responsabilities:
-        </p>
-        <ul class="list-disc pl-3 md:pl-8 text-xs md:text-sm xl:text-lg">
+      <p class="mt-[15px] text-3xl text-black italic font-bold">For {{ work.client }}</p>
+      <div class="mt-[30px] text-black bg-white p-3">
+        <p class="text-xs font-semibold">{{ work.description }}</p>
+        <p class="mt-2 text-xs underline underline-offset-4 decoration-dotted">Responsabilities:</p>
+        <ul class="text-xs list-disc mt-2 ml-3">
           <li v-for="responsability in work.responsabilities" v-bind:key="responsability">
             {{ responsability }}
           </li>
         </ul>
       </div>
-      <ul
-        class="absolute justify-around flex-wrap lg:flex-nowrap left-[30px] right-[30px] bottom-[30px] md:left-[10%] md:right-[10%] md:bottom-[8%] lg:left-[initial] lg:right-[10%] lg:bottom-[8%] xl:right-[15%] xl:bottom-[12%] text-[14px] md:text-2xl text-white flex flex-row py-2 px-6 bg-black"
-      >
+      <ul class="text-md text-white mt-auto justify-around flex flex-wrap py-2 px-6 bg-black">
         <li v-for="tag in work.tags" v-bind:key="tag" class="mr-4 last:mr-0">
           {{ tag }}
         </li>
