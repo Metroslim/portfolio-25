4c6c5b6f2448a8bdaa486becf7babd2613e9785a
Arnaud <arnaud.masson@gimbalcube.com>
2025-03-24 15:04:00 +0100

    changing all px tailwind class to rem

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -125,21 +125,27 @@ function render() {
 
 <template>
   <section class="w-full h-dvh snap-always snap-start">
-    <div class="w-full h-full fixed justify-start items-start flex flex-col pointer-events-none">
-      <div class="max-w-5xl w-full pt-8 px-8 md:pt-16 md:px-14 flex flex-col">
-        <h1 class="text-6xl md:text-7xl font-bold">Arnaud Masson</h1>
-        <h2 class="text-4xl md:text-5xl font-bold text-purple-500 mt-2">Senior Unity Developer</h2>
-        <h3 class="text-2xl md:text-3xl font-semibold mt-6">MR, VR, AR, Interactive Experiences</h3>
-      </div>
-      <div class="self-end mt-20 md:mr-[120px] md:mt-[240px]">
-        <a
-          class="flex flex-row items-center text-sm md:text-lg font-semibold py-3 px-4 bg-gradient-to-r from-purple-500 to-pink-500 text-white pointer-events-auto"
-          href="./pdf/Resume - Arnaud Masson - 2025.pdf"
-          target="_blank"
-        >
-          <RightArrowSVG class="h-6 w-4 md:h-8 md:w-6 md:mr-2 fill-white" />
-          <span class="flex-initial">Download resume</span>
-        </a>
+    <div class="w-full h-full fixed pointer-events-none">
+      <div class="w-full max-w-5xl mx-auto h-full justify-start items-start flex flex-col">
+        <div class="w-full pt-8 px-8 md:pt-16 md:px-14 lg:pt-[8rem] flex flex-col">
+          <h1 class="text-6xl md:text-7xl font-bold">Arnaud Masson</h1>
+          <h2 class="text-4xl md:text-5xl font-bold text-purple-500 mt-2">
+            Senior Unity Developer
+          </h2>
+          <h3 class="text-2xl md:text-3xl font-semibold mt-6">
+            MR, VR, AR, Interactive Experiences
+          </h3>
+        </div>
+        <div class="self-end mt-20 md:mr-[8rem] md:mt-[12rem]">
+          <a
+            class="flex flex-row items-center text-sm md:text-lg font-semibold py-3 px-4 bg-gradient-to-r from-purple-500 to-pink-500 text-white pointer-events-auto"
+            href="./pdf/Resume - Arnaud Masson - 2025.pdf"
+            target="_blank"
+          >
+            <RightArrowSVG class="h-6 w-4 md:h-8 md:w-6 md:mr-2 fill-white" />
+            <span class="flex-initial">Download resume</span>
+          </a>
+        </div>
       </div>
       <div class="absolute top-0 left-0 w-full h-full z-[-1] bg-white opacity-60"></div>
       <div id="marchinecube-container" class="absolute top-0 left-0 w-full h-full z-[-5]"></div>
--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -11,19 +11,19 @@ defineProps<{
     class="item relative h-full w-full bg-gradient-to-r group-odd:from-purple-500 group-odd:to-pink-500 group-even:from-sky-500 group-even:to-indigo-500 overflow-hidden"
   >
     <img
-      class="absolute p-[15px] md:p-[30px] h-full w-full object-cover z-0"
+      class="absolute p-[1rem] md:p-[2rem] h-full w-full object-cover z-0"
       :src="work.backgroundImagePath"
       alt="Work Item Background"
       name="background"
     />
-    <div class="p-[30px] md:p-[60px] h-full w-full relative z-5 flex flex-col">
+    <div class="p-[2rem] md:p-[4rem] h-full w-full relative z-5 flex flex-col">
       <h3 class="text-4xl md:text-5xl text-white font-bold">
         {{ work.name }}
       </h3>
-      <p class="mt-[15px] md:mt-[30px] text-3xl md:text-4xl text-black italic font-bold">
+      <p class="mt-[1rem] md:mt-[2rem] text-2xl md:text-3xl text-black italic font-bold">
         For {{ work.client }}
       </p>
-      <div class="mt-[30px] md:mt-[60px] p-3 md:p-5 lg:max-w-xl text-black bg-white">
+      <div class="mt-[2rem] md:mt-[4rem] p-3 md:p-5 lg:max-w-xl text-black bg-white">
         <p class="text-xs md:text-sm font-semibold">{{ work.description }}</p>
         <p class="mt-2 md:mt-6 text-xs md:text-sm underline underline-offset-4 decoration-dotted">
           Responsabilities:
